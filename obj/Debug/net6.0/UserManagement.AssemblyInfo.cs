faf7c4a8665066ff044a4ae8dfcba3cbc124af89
Mahesh Singh Madai <maheshmadai977@gmail.com>
2024-07-30 13:17:20 +0545

    gitignore update

--- obj/Debug/net6.0/UserManagement.AssemblyInfo.cs
+++ obj/Debug/net6.0/UserManagement.AssemblyInfo.cs
@@ -11,12 +11,11 @@
 using System;
 using System.Reflection;
 
-[assembly: Microsoft.AspNetCore.Identity.UI.UIFrameworkAttribute("Bootstrap5")]
 [assembly: Microsoft.Extensions.Configuration.UserSecrets.UserSecretsIdAttribute("aspnet-UserManagement-80B76B2A-7B75-40EB-8882-44F11C1C98F1")]
 [assembly: System.Reflection.AssemblyCompanyAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92483a5018e406a86bf28fc61cc6b56c835488f6")]
 [assembly: System.Reflection.AssemblyProductAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UserManagement")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
